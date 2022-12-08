6a0fa6feef084173e724706cb2165b0ae14b894a
Victor Payno <vpayno@users.noreply.github.com>
2022-12-07 22:55:07 -0800

    build: add installation script

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+if [[ ! -d helix.git/ ]]; then
+	printf "ERROR: helix.git submodule doesn't exist.\n"
+	printf "\n"
+	exit 1
+fi
+
+echo Running: cd helix.git/
+cd helix.git/ || exit
+printf "\n"
+
+echo Running: git restore .
+git restore .
+printf "\n"
+
+echo Running: git switch master
+git switch master || exit
+printf "\n"
+
+echo Running: git pull
+time git pull || exit
+printf "\n"
+
+printf -v hx_tag "%s" "$(git tag | grep -E '^[2-9][0-9][.][0-9]+([.][0-9]+)?$' | tail -n 1)"
+printf "Latest helix tag: %s\n" "${hx_tag}"
+printf "\n"
+
+echo Running: git switch --detach "${hx_tag}"
+git switch --detach "${hx_tag}" || exit
+printf "\n"
+
+echo Running: cargo install --path helix-term
+time cargo install --path helix-term || exit
+printf "\n"
+
+echo Running: hx --version
+hx --version || exit
+printf "\n"
+
+# echo Running: hx --grammar fetch
+# hx --grammar fetch
+# printf "\n"
+
+# echo Running: hx --grammar build
+# hx --grammar build
+# printf "\n"
