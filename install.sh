8d510ddfdc91ee686976e160aadd1e957ba13175
Victor Payno <vpayno@users.noreply.github.com>
2024-01-24 21:38:21 -0800

    build: add option to build from head instead of latest tag

--- install.sh
+++ install.sh
@@ -6,6 +6,12 @@ if [[ ! -d helix.git/ ]]; then
 	exit 1
 fi
 
+declare USE_HEAD=false
+
+if [[ ${1} == head ]]; then
+	USE_HEAD=true
+fi
+
 declare hx_tag
 declare hx_ver
 
@@ -33,23 +39,30 @@ echo Running: git pull
 time git pull || exit
 printf "\n"
 
-printf -v hx_tag "%s" "$(git tag | grep -E '^[2-9][0-9][.][0-9]+([.][0-9]+)?$' | tail -n 1)"
+if ${USE_HEAD}; then
+	printf -v hx_tag "%s" "$(git describe --tags --long | sed -r -e 's/^([0-9]+\.[0-9]+)-[0-9]+-([a-z0-9]+)$/\1/g')"
+else
+	printf -v hx_tag "%s" "$(git tag | grep -E '^[2-9][0-9][.][0-9]+([.][0-9]+)?$' | tail -n 1)"
+fi
+
 printf "Latest helix tag: %s\n" "${hx_tag}"
 printf "\n"
 
-echo Running: git switch --detach "${hx_tag}"
-git switch --detach "${hx_tag}" || exit
-printf "\n"
+if ! ${USE_HEAD}; then
+	echo Running: git switch --detach "${hx_tag}"
+	git switch --detach "${hx_tag}" || exit
+	printf "\n"
+
+	# temporary fix for the 23.10 tag
+	if [[ ${hx_tag} == 23.10 ]]; then
+		echo Running: git cherry-pick 6d168eda275deb23b0c643aecd746af3f4cc9937
+		git cherry-pick 6d168eda275deb23b0c643aecd746af3f4cc9937
+	fi
+fi
 
 echo Running: rustup override set stable
 rustup override set stable
 printf "\n"
-
-# temporary fix for the 23.10 tag
-if [[ ${hx_tag} == 23.10 ]]; then
-	echo Running: git cherry-pick 6d168eda275deb23b0c643aecd746af3f4cc9937
-	git cherry-pick 6d168eda275deb23b0c643aecd746af3f4cc9937
-fi
 
 echo Running: cargo install --path helix-term
 time cargo install --path helix-term || exit
@@ -59,7 +72,7 @@ echo Running: hx --version
 hx --version || exit
 printf "\n"
 
-hx_ver="$(hx --version | awk '/^helix/ { print $2 }')"
+printf -v hx_ver "%s" "$(hx --version | awk '/^helix/ { print $2 }')"
 
 if [[ ${hx_ver} == "${hx_tag}" ]]; then
 	printf "Success: installed hx has the expected version (got:%s == want:%s)\n" "${hx_ver}" "${hx_tag}"
